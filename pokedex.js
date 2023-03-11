16db3145aa0de9f1744869a1d780844e0d63bb04
javierz95 <javierzurita95@gmail.com>
2023-03-11 17:16:26 +0100

    basic styles added

--- pokedex.js
+++ pokedex.js
@@ -25,11 +25,11 @@ const drawPoke = (content) => {
     divPokemon$$.innerHTML = `
     <div class="pokemon">
             <div class="pokemon__header">
-                <h3>${content.name}</h3>   
-                <p>${content.id}</p>
+            <div class="pokemon__header--p"><p>${content.id}</p></div>
+            <div class="pokemon__header--img"><h3>${content.name}</h3></div>       
             </div>
             <div class="pokemon__img">
-                <img src="${content.sprites.front_default}" alt="imagen-${content.name}">
+                <img src="${content.sprites.front_default}" alt="imagen-${content.name}" class="image--poke">
             </div>
             <div class="pokemon__types">
                 <div class="pokemon__types1">
@@ -44,6 +44,21 @@ const drawPoke = (content) => {
         </div>
     `
 
+    let pType1$$ = document.querySelector('.pokemon__types1--p');
+    let pType2$$ = document.querySelector('.pokemon__types2--p');
+    let pImg1$$ = document.querySelector('.pokemon__imgs1--p');
+    let pImg2$$ = document.querySelector('.pokemon__imgs2--p');
+
+
+  /*  
+    if(content.types.length >= 2){
+       console.log(content.types.length);
+       console.log("Tipo 1:"+content.types[0].type.name);
+       console.log("Tipo 2:"+content.types[1].type.name);
+
+        pType2$$.textContent = content.types[1].type.name;
+    }
+*/
     divPokemonList$$.appendChild(divPokemon$$);
 }
 
