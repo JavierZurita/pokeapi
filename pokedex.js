39dd739f893eba4e08fd5f88b7b7b4dd30d071e3
Usuario <javierzurita95@gmail.com>
2023-03-12 00:45:59 +0100

    basic front done

--- pokedex.js
+++ pokedex.js
@@ -1,4 +1,7 @@
 
+const divPokemonList$$ = document.querySelector('.pokemon--list');
+const input$$ = document.querySelector('input');
+const pokeArray = [];
 const getPokemon = async () => {
 
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151");
@@ -12,54 +15,56 @@ const getPokemon = async () => {
         const responseJson1 = await response1.json();
      
         console.log(responseJson1.name);
-        drawPoke(responseJson1);
+       drawPoke(responseJson1);
+       pokeArray.push(responseJson1);
     }
 }
 
-    const divPokemonList$$ = document.querySelector('.pokemon--list');
 
 const drawPoke = (content) => {
 
-    let divPokemon$$ = document.createElement('div');
-    divPokemon$$.className = "pokemon";
-    divPokemon$$.innerHTML = `
-    <div class="pokemon">
+    let html = `
+    <div class="pokemon zoom">
             <div class="pokemon__header">
-            <div class="pokemon__header--p"><p>${content.id}</p></div>
-            <div class="pokemon__header--img"><h3>${content.name}</h3></div>       
+            <div class="pokemon__header--p"><p>#${content.id.toString().padStart(3,0)}</p></div>
+            <div class="pokemon__header--img"><h3>${content.name[0].toUpperCase()}${content.name.slice(1)}</h3></div>       
             </div>
-            <div class="pokemon__img">
+            <div class="pokemon__img zoom">
                 <img src="${content.sprites.front_default}" alt="imagen-${content.name}" class="image--poke">
             </div>
-            <div class="pokemon__types">
-                <div class="pokemon__types1">
-                    <p class="pokemon__types1--p">${content.types[0].type.name}</p>
-                    <img src="" alt="" class="pokemon__types1--img">
-                </div>
-                <div class="pokemon__types2">
-                    <p class="pokemon__types2--p"></p>
-                    <img src="" alt="" class="pokemon__types2--img">
-                </div>
-            </div>
+            <div class="pokemon__types">`
+
+    for (let i = 0; i < content.types.length; i++){
+                html += `<div class="pokemon__types--div">
+                            <div class="pokemon__types--icon ${content.types[i].type.name}--text "></div>
+                            <div class="pokemon__types--icon ${content.types[i].type.name}--icon"></div>
+                        </div> `
+                }
+    html += `
         </div>
     `
 
-    let pType1$$ = document.querySelector('.pokemon__types1--p');
-    let pType2$$ = document.querySelector('.pokemon__types2--p');
-    let pImg1$$ = document.querySelector('.pokemon__imgs1--p');
-    let pImg2$$ = document.querySelector('.pokemon__imgs2--p');
+    let divPokemon$$ = document.createElement('div');
+    divPokemon$$.className = "pokemon";
+    divPokemon$$.innerHTML = html;
 
-
-  /*  
-    if(content.types.length >= 2){
-       console.log(content.types.length);
-       console.log("Tipo 1:"+content.types[0].type.name);
-       console.log("Tipo 2:"+content.types[1].type.name);
-
-        pType2$$.textContent = content.types[1].type.name;
-    }
-*/
     divPokemonList$$.appendChild(divPokemon$$);
 }
 
+const filter = () =>{
+
+    divPokemonList$$.innerHTML="";
+
+    console.log(input$$.value);
+        
+        for (let i = 0; i < pokeArray.length; i++) {
+            if(pokeArray[i].name.includes(input$$.value.toLowerCase())){
+                drawPoke(pokeArray[i]);
+            }
+        }    
+} 
+
+
 getPokemon();
+
+input$$.addEventListener('input',filter);
