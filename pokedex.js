1f16d1f8a39e63b1d7a8e9ac96153df36288f6b5
Usuario <javierzurita95@gmail.com>
2023-03-16 10:43:40 +0100

    2.0 version

--- pokedex.js
+++ pokedex.js
@@ -1,26 +1,31 @@
-let baseUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151";
+let baseUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1008";
 
 const divPokemonList$$ = document.querySelector('.pokemon--list');
 const inputName$$ = document.querySelector('.filter--name');
 const inputType$$ = document.querySelector('#filter--type');
-const btnnext$$ = document.querySelector('.btn--next');
+const inputGen$$ = document.querySelector('#filter--gen');
+const btnNxt$$ = document.querySelector('.btn--nxt');
+const btnPrev$$ = document.querySelector('.btn--prev');
+let page = 1;
 const pokeArray = [];
 
 const init = async (url) => {
-
+    btnPrev$$.className = "empty";
     const response = await fetch(url);
     const responseJson = await response.json();
-
+    
     for (let i = 0; i < responseJson.results.length; i++) {
         
         const response1 = await fetch(responseJson.results[i].url);
         const responseJson1 = await response1.json();
-        drawPoke(responseJson1);
         pokeArray.push(responseJson1);
+        if(i<151){
+            drawPoke(pokeArray[i]);
+        }
     }
+
     console.log(pokeArray);
 }
-
 
 const drawPoke = (content) => {
 
@@ -46,7 +51,7 @@ const drawPoke = (content) => {
             <div class="pokemon__img">
                 <img src="${content.sprites.front_default}" alt="imagen-${content.name}" class="image--poke">
             </div>
-            <div class="pokemon__name"><h3>${content.name[0].toUpperCase()}${content.name.slice(1)}</h3></div>  
+            <div class="pokemon__name"><h4>${content.name[0].toUpperCase()}${content.name.slice(1)}</h4></div>  
             <div class="pokemon__types">`;
 
     for (let i = 0; i < content.types.length; i++){
@@ -105,7 +110,8 @@ const drawPoke = (content) => {
     divPokemonList$$.appendChild(divPokemon$$);
 }
 
-const filter = () =>{
+const filter = () => {
+    inputGen$$.value = 0;
     divPokemonList$$.innerHTML="";
 
     for (let i = 0; i < pokeArray.length; i++) {
@@ -122,8 +128,203 @@ const filter = () =>{
     }
 }
 
+const filterGen = () => {
+    inputType$$.value = "";
+    divPokemonList$$.innerHTML="";
+    console.log(inputGen$$.value);
+
+    if(inputGen$$.value == 1){
+        page == 1;
+            btnNxt$$.className = "btn--nxt";
+            btnPrev$$.className = "empty";
+            for(let i = 0; i < 151; i++){ //check this
+                drawPoke(pokeArray[i]);
+            }
+    } else if (inputGen$$.value == 2 ){
+        btnPrev$$.className = "btn--prev";
+        btnNxt$$.className = "btn--nxt";
+
+        console.log("Enrta 2");
+
+            page == 2;
+            for(let i = 151; i < 251; i++){ //check this
+                drawPoke(pokeArray[i]);
+            }
+    } else if (inputGen$$.value == 3){
+        btnPrev$$.className = "btn--prev";
+        btnNxt$$.className = "btn--nxt";
+
+            page == 3;
+            for(let i = 251; i < 386; i++){
+                drawPoke(pokeArray[i]);
+            }
+    } else if (inputGen$$.value == 4){
+        btnPrev$$.className = "btn--prev";
+        btnNxt$$.className = "btn--nxt";
+
+        page == 4;
+        for(let i = 386; i < 494; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (inputGen$$.value == 5){
+        btnPrev$$.className = "btn--prev";
+        btnNxt$$.className = "btn--nxt";
+
+        page = 5;
+            for(let i = 494; i < 649; i++){
+                drawPoke(pokeArray[i]);
+            }
+    } else if (inputGen$$.value == 6){
+        btnPrev$$.className = "btn--prev";
+        btnNxt$$.className = "btn--nxt";
+
+        page = 6;    
+            for(let i = 649; i < 721; i++){
+                drawPoke(pokeArray[i]);
+            }
+    } else if (inputGen$$.value == 7){
+        btnPrev$$.className = "btn--prev";
+        btnNxt$$.className = "btn--nxt";
+
+        page = 7;
+        for(let i = 721; i < 809; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (inputGen$$.value == 8){
+        btnPrev$$.className = "btn--prev";
+        btnNxt$$.className = "btn--nxt";
+        page = 8;
+            for(let i = 809; i < 905; i++){
+                drawPoke(pokeArray[i]);
+            }
+    } else if (inputGen$$.value == 9){
+            btnPrev$$.className = "btn--prev";
+             btnNxt$$.className = "empty";
+            page = 9;
+
+            for(let i = 905; i < 1009; i++){
+                drawPoke(pokeArray[i]);
+            }
+    }else if (inputGen$$.value == 0 ){
+            page == 1;
+        btnNxt$$.className = "btn--nxt";
+        btnPrev$$.className = "empty"
+            for(let i = 0; i < 1009; i++){ //check this
+                drawPoke(pokeArray[i]);
+            }
+    }
+}
+const nextPage = () => {
+    console.log(page);
+    page += 1;
+
+    if(page == 2){
+        btnPrev$$.className = "btn--prev";
+        divPokemonList$$.innerHTML = "";
+        for(let i = 151; i < 251; i++){ //check this
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 3){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 251; i < 386; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 4){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 386; i < 494; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 5){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 494; i < 649; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 6){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 649; i < 721; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 7){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 721; i < 809; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 8){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 809; i < 905; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 9){
+        divPokemonList$$.innerHTML = "";
+        btnNxt$$.className = "empty";
+        for(let i = 905; i < 1009; i++){
+            drawPoke(pokeArray[i]);
+        } 
+    }
+    window.scrollTo({top:0})
+}
+
+const prevPage = () => {
+    console.log(page);
+    page = page - 1;
+
+    if(page == 1){
+        btnPrev$$.className = "empty";
+        divPokemonList$$.innerHTML = "";
+        for(let i = 0; i < 151; i++){ //check this
+            drawPoke(pokeArray[i]);
+        }
+    } else if(page == 2){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 151; i < 251; i++){ //check this
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 3){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 251; i < 386; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 4){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 386; i < 494; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 5){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 494; i < 649; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 6){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 649; i < 721; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 7){
+        divPokemonList$$.innerHTML = "";
+        for(let i = 721; i < 809; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 8){
+        btnNxt$$.className = "btn--nxt";
+        divPokemonList$$.innerHTML = "";
+        for(let i = 809; i < 905; i++){
+            drawPoke(pokeArray[i]);
+        }
+    } else if (page == 9){
+        divPokemonList$$.innerHTML = "";
+        btnNxt$$.className = "empty";
+        for(let i = 905; i < 1009; i++){
+            drawPoke(pokeArray[i]);
+        }
+    }
+    window.scrollTo({top:0})
+
+}
 
 init(baseUrl);
 
+inputGen$$.addEventListener('input',filterGen);
 inputName$$.addEventListener('input',filter);
 inputType$$.addEventListener('input',filter);
+btnNxt$$.addEventListener('click', nextPage);
+btnPrev$$.addEventListener('click', prevPage);
